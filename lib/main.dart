893ebf315e08adfec4300276c90e4cf99f031676
elliottxharris <elliottxharris@gmail.com>
2023-11-27 17:58:14 -0600

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_kitchen/Pages/home.dart';
 import 'package:my_kitchen/Pages/ingredients.dart';
@@ -5,8 +6,11 @@ import 'package:my_kitchen/Pages/recipes.dart';
 import 'package:my_kitchen/Pages/settings.dart';
 import 'package:my_kitchen/Pages/tab_bar_pages.dart';
 import 'package:my_kitchen/Themes/theme.dart';
+import 'package:my_kitchen/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
